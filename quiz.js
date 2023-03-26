c7d54edc91c78f65291a7ef7e009bbc8f03d142a
ishanm17 <mulchandaniishan@gmail.com>
2023-03-26 13:42:27 +0530

    completed quiz.js

--- quiz.js
+++ quiz.js
@@ -1,28 +1,47 @@
-function check()
-{
-    var count = 0;
+const answers = [
+    [],
+    [],
+    [],
+    ["b", "a", "b", "d", "a"],
+    [],
+    [],
+    [],
+    [],
+];
 
-    // question 1 
-    var ans1 = document.getElementsByClassName("q1");
-    for(let i = 0 ;i<ans1.length;i++)
-    {
-        if(ans1[i].checked)
-        {
-            console.log(ans1[i].value);
-            if(ans1[i].value == 3)
-            {
-                document.getElementById("result1").innerHTML = "correct "
-                count = count + 1;
-            }
-            else
-            {
-                document.getElementById("result1").innerHTML = "incorrect"
+//0=segregation, 1=landfills, 2=solid, 3=hazardous, 4=radioactive, 5=ewaste, 6=wastewater, 7=biomedical 
+
+function displayResult(index){
+    let correctAnswers = answers[index];
+    let userAnswers = [];
+    let qDiv;
+    let question;
+    let score = 0;
+    for(let i=1; i<=5; i++){
+        qDiv = document.getElementById("question" + i);
+        question = document.getElementsByName("a" + i);
+        for(let j=0; j<question.length; j++){
+            if(question[j].checked){
+                userAnswers.push(question[j].value);  
+                break;
             }
         }
     }
+    console.log(userAnswers);
+    console.log(correctAnswers);
+    for(let i=0; i<userAnswers.length; i++){
+        if(userAnswers[i] !== correctAnswers[i]){
+            document.getElementById("question" + (i+1)).style.backgroundColor = "#CD5C5C";
+        }
+        else{
+            document.getElementById("question" + (i+1)).style.backgroundColor = "";
+            score++;
+        }
+    }
 
-    qustion 
-
-    document.getElementById("re").innerHTML= count;
-    
+    let result = document.getElementsByClassName("result")[0];
+    let resultText = document.createElement("h1");
+    resultText.innerHTML = "Score: " + score + "/5";
+    resultText.classList.add("result-text");
+    result.prepend(resultText);
 }
